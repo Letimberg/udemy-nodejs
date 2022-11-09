b3e6b274223b2f859761883a8d952d2d7b1c08af
letimberg.cavalcante <letimberg.cavalcante@hemoce.ce.gov.br>
2022-11-09 15:25:55 -0300

    [started] #1 - Estudos em Node.js

---
+++ inicio/app.js
@@ -0,0 +1,10 @@
+var mostrarSite = true; // false
+const site = "www.guiadoprogramador.com"
+
+console.log("Hello World!")
+console.log("Meu nome é Letimberg!")
+console.log("E eu estou aprendendo Node.js com o Guia do programador")
+
+if(mostrarSite) { // Se mostrar site
+  console.log(site) // Printe o site
+}
